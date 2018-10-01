e141d71097d89780e2075f9182ddf73e73371947
Aadit Kamat <aadit.k12@gmail.com>
2018-10-02 01:20:31 +0800

    Add transaction component to add command and its parser
    Signed-off-by: aaditkamat <aadit.k12@gmail.com>

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -11,8 +11,7 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
-
-
+import seedu.address.model.transaction.Transaction;
 
 
 /**
@@ -38,36 +37,44 @@ public class AddCommand extends Command {
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
-    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_PERSON_ADDED_SUCCESS = "New person added: %1$s";
+    public static final String MESSAGE_TRANSACTION_ADDED_SUCCESS = "New transaction added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the database";
+    public static final String MESSAGE_DUPLICATE_TRANSACTION = "This transaction already exists in the database";
 
-    private final Person toAdd;
+    private final Person newPerson;
+    private final Transaction newTransaction;
 
     /**
      * Creates an AddCommand to add the specified {@code Person}
      */
-    public AddCommand(Person person) {
+    public AddCommand(Person person, Transaction transaction) {
         requireNonNull(person);
-        toAdd = person;
+        requireNonNull(transaction);
+
+        newPerson = person;
+        newTransaction = transaction;
     }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
-        if (model.hasPerson(toAdd)) {
+        if (model.hasPerson(newPerson)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
 
-        model.addPerson(toAdd);
+        model.addPerson(newPerson);
+
         model.commitAddressBook();
-        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+        return new CommandResult(String.format(MESSAGE_PERSON_ADDED_SUCCESS, newPerson)
+                                       .concat(String.format(MESSAGE_TRANSACTION_ADDED_SUCCESS, newTransaction)));
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof AddCommand // instanceof handles nulls
-                && toAdd.equals(((AddCommand) other).toAdd));
+                && newPerson.equals(((AddCommand) other).newPerson));
     }
 }
