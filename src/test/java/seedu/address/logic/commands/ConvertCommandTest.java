813068f220091a8b0a2f76161536c0af32b60762
aaditkamat <aadit.k12@gmail.com>
2018-10-26 00:28:43 +0800

    Change the deadline date in the xml to avoid date conflicts
    Signed-off-by: Aadit <aadit.k12@gmail.com>

--- src/test/java/seedu/address/logic/commands/ConvertCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ConvertCommandTest.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.commands.CommandTestUtil.AMOUNT_DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 import java.util.Arrays;
