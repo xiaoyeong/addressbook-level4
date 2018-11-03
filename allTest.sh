798498201d15418395723f6d9cd8e81b87149ac1
aaditkamat <aadit.k12@gmail.com>
2018-11-03 23:24:17 +0800

    Remove trailing whitespaces
    Signed-off-by: Aadit <aadit.k12@gmail.com>

--- allTest.sh
+++ allTest.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-source ~/.profile
 ./config/travis/run-checks.sh &&
 ./gradlew clean checkstyleMain checkstyleTest headless allTests coverage coveralls asciidoctor copyDummySearchPage
