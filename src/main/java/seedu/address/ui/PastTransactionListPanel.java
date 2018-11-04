a9718ad019d0f89b9aed2134819b20f93088f1d2
DESKTOP-2HNCDPM\Ysabella <e0201472@u.nus.edu>
2018-11-04 16:16:05 +0800

    fixing checkstyle errors

--- src/main/java/seedu/address/ui/PastTransactionListPanel.java
+++ src/main/java/seedu/address/ui/PastTransactionListPanel.java
@@ -3,7 +3,9 @@ package seedu.address.ui;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
+
 import javafx.application.Platform;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
