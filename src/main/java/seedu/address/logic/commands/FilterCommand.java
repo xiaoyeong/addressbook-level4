ee64433d098ed755801624235e8905139acbe5f2
julianc269 <julian_269@hotmail.com>
2018-11-11 16:23:35 +0800

    FilterCommand: remove commiting to database

--- src/main/java/seedu/address/logic/commands/FilterCommand.java
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -68,7 +68,6 @@ public class FilterCommand extends Command {
         MultiFieldPredicate predicate = new MultiFieldPredicate(predicates, operatorType);
         model.updateFilteredTransactionList(predicate);
         model.updateFilteredPastTransactionList(predicate);
-        model.commitFinancialDatabase();
         return new CommandResult(
                 String.format(Messages.MESSAGE_ALL_TRANSACTIONS_LISTED_OVERVIEW,
                         model.getFilteredTransactionList().size(), model.getFilteredPastTransactionList().size()));
