f2705794e04f08fd3ba774f1a89685e9f086a72c
julianc269 <julian_269@hotmail.com>
2018-11-11 14:23:45 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -41,61 +41,61 @@ public class FilterCommandParserTest {
     }
 
     @Test
-    public void multipleIdenticalPrefixes_throwsParseException(){
+    public void multipleIdenticalPrefixes_throwsParseException() {
         assertParseFailure(parser, " e/example.com e/gmail.com", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 FilterCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void prefixWithEmptyKeywords_throwsParseException(){
+    public void prefixWithEmptyKeywords_throwsParseException() {
         assertParseFailure(parser, " n/", MESSAGE_KEYWORDS_NONEMPTY);
         assertParseFailure(parser, " e/;;", MESSAGE_KEYWORDS_NONEMPTY);
     }
 
     @Test
-    public void parse_operatorPrefixOnly_throwsParseException(){
+    public void parse_operatorPrefixOnly_throwsParseException() {
         assertParseFailure(parser, " or/", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                 FilterCommand.MESSAGE_USAGE));
     }
 
     @Test
-    public void parse_twoOperators_throwsParseException(){
+    public void parse_twoOperators_throwsParseException() {
         assertParseFailure(parser, " n/alice e/example.com or/ and/",
                 String.format(MESSAGE_INVALID_PREFIX_COMBINATION, PREFIX_OR, PREFIX_AND));
     }
 
     @Test
-    public void parse_orOperatorHasValue_throwsParseException(){
+    public void parse_orOperatorHasValue_throwsParseException() {
         assertParseFailure(parser, " n/alice e/example.com or/value",
                 String.format(MESSAGE_INVALID_PREFIX_VALUE, PREFIX_OR));
     }
 
     @Test
-    public void parse_andOperatorHasValue_throwsParseException(){
+    public void parse_andOperatorHasValue_throwsParseException() {
         assertParseFailure(parser, " n/alice e/example.com and/value",
                 String.format(MESSAGE_INVALID_PREFIX_VALUE, PREFIX_AND));
     }
 
     @Test
-    public void parse_deadlineMaxBeforeDeadlineMin_throwsParseException(){
+    public void parse_deadlineMaxBeforeDeadlineMin_throwsParseException() {
         assertParseFailure(parser, " tdmin/10/11/2018 tdmax/10/10/2018",
                 MESSAGE_LATESTDEADLINE_BEFORE_EARLIESTDEADLINE);
     }
 
     @Test
-    public void parse_amountMaxLessThanAmountMin_throwsParseException(){
+    public void parse_amountMaxLessThanAmountMin_throwsParseException() {
         assertParseFailure(parser, " tamin/50.00 tamax/10.00",
                 MESSAGE_MAXAMOUNT_LESSTHAN_MINAMOUNT);
     }
 
     @Test
-    public void parse_invalidTransactionAmountBound_throwsParseException(){
+    public void parse_invalidTransactionAmountBound_throwsParseException() {
         assertParseFailure(parser, " tamax/SGD 50.00",
                 MESSAGE_TRANSACTION_AMOUNT_BOUND_CONSTRAINT);
     }
 
     @Test
-    public void parse_invalidMinTransactionAmount_throwsParseException(){
+    public void parse_invalidMinTransactionAmount_throwsParseException() {
         assertParseFailure(parser, " tamin/SGD 50.00",
                 MESSAGE_TRANSACTION_AMOUNT_BOUND_CONSTRAINT);
     }
